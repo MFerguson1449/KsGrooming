e926c7d2fdee8afd178006b2741286889c5e4485
MFerguson1449 <MFerguson1449@gmail.com>
2019-08-24 11:02:46 -0400

    service layout

--- dist/icon/manifest.js
+++ dist/icon/manifest.js
@@ -180,7 +180,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14602" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "17067" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
